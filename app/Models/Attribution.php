6a650a555f63fa53d3731680bdb25c0c699f4aeb
g0tie <gaultierbegue@outlook.com>
2021-03-10 19:10:18 +0400

    First commit

---
+++ app/Models/Attribution.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribution extends Model
+{
+    use HasFactory;
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'computer_id',
+        'starting_date',
+        'expiration_date'
+    ];
+}
